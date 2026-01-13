56d51489db6ee405e191ccc2d065224a56923625
xsharawi <xsharawi@gmail.com>
2026-01-13 04:08:47 +0200

    fake

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+
+def main():
+    _ = pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        _ = screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
